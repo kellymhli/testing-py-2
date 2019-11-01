3f0708c48dff03e84773517049a7f3b2445c1584
Your Name <you@example.com>
2019-11-01 16:01:36 -0700

    Write test for successful RSVP

--- tests.py
+++ tests.py
@@ -32,7 +32,8 @@ class PartyTests(unittest.TestCase):
                                   follow_redirects=True)
         # FIXME: Once we RSVP, we should see the party details, but
         # not the RSVP form
-        print("FIXME")
+        self.assertIn(b"Party Details", result.data)
+        self.assertNotIn(b"Please RSVP", result.data)
 
 
 class PartyTestsDatabase(unittest.TestCase):
